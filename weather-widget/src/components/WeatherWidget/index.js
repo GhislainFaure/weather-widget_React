87b867648b554fac1d288b92e035e1d1b1308577
GhislainFaure <ghislainfaure06@gmail.com>
2023-12-30 16:36:50 +0100

    utilisation useEffect pour le fetch API + gestion du submit

--- weather-widget/src/components/WeatherWidget/index.js
+++ weather-widget/src/components/WeatherWidget/index.js
@@ -1,11 +1,24 @@
 import "./style.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function WeatherWidget() {
   const [city, setCity] = useState("Monaco");
   const [desc, setDesc] = useState("temps orange");
   const [temperature, setTemperature] = useState("10");
   const [inputValue, setInputValue] = useState("");
+
+  const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+  const apikey = "3f31e39676524d90deea984395031598";
+  useEffect(() => {
+    fetch(`${baseUrl}?q=${city}&units=metric&appid=${apikey}&lang=fr`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDesc(data.weather[0].description);
+        const temp = Math.round(data.main.temp);
+        setTemperature(temp);
+      })
+      .catch((error) => console.log(error));
+  }, [city]);
 
   // const handleSubmit = () => {};
   return (
@@ -18,16 +31,24 @@ export default function WeatherWidget() {
         <span>{temperature}</span>
         <sup>°C</sup>
       </p>
-      <input
-        className="weather-widget__input"
-        type="text"
-        placeholder="Votre Ville"
-        value={inputValue}
-        onChange={(event) => {
-          setInputValue(event.target.value);
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setCity(inputValue);
+          setInputValue("");
         }}
-        // onSubmit={handleSubmit}
-      />
+        className="weather-widget__form"
+      >
+        <input
+          className="weather-widget__input"
+          type="text"
+          placeholder="Votre Ville"
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        />
+      </form>
     </div>
   );
 }
