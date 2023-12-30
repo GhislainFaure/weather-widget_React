0c7abe944ff655e5aadb9b75a3c80442785d5158
GhislainFaure <ghislainfaure06@gmail.com>
2023-12-30 03:17:36 +0100

    JSX + styles

--- weather-widget/src/components/WeatherWidget/index.js
+++ weather-widget/src/components/WeatherWidget/index.js
@@ -1,5 +1,22 @@
 import "./style.scss";
 
 export default function WeatherWidget() {
-  return <div className="weather-widget">Weather Widget</div>;
+  return (
+    <div className="weather-widget">
+      <div className="weather-widget__infos">
+        <p className="weather-widget__city">Monaco</p>
+        <p className="weather-widget__desc">temps saharien</p>
+      </div>
+      <p className="weather-widget__temperature">
+        <span>25</span>
+        <sup>°C</sup>
+      </p>
+      <input
+        className="weather-widget__input"
+        type="text"
+        placeholder="Votre Ville"
+        value={null}
+      />
+    </div>
+  );
 }
